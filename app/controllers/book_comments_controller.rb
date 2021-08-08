a2f20cf1dee81ff2efe3d6ed46ca38e01ea2f961
ogiyam <miyuogym@gmail.com>
2021-08-08 13:16:21 +0000

    [Finish]Bookers-task6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,14 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.save
+    @book_comment_new = BookComment.new
     render :index
   end
 
 
   def destroy
     @book = Book.find(params[:book_id])
+    @book_comment_new = BookComment.new
     @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     render :index
   end
