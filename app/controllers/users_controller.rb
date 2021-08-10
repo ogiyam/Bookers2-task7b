76b42db233a2523e6b08b5ced374c7906349e16e
ogiyam <miyuogym@gmail.com>
2021-08-10 18:21:38 +0000

    [Fix]Bookers2-task7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @new_book = Book.new
     @books = @user.books
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
